5890630c3f08501f7b1a25c606bbd570e6162f2c
ErikPtyn <erik.petrosyan.pro@gmail.com>
2025-12-05 23:04:05 +0100

    Data from COF

--- deal-data.py
+++ deal-data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import yfinance as yf
 
-TICKERS = ["COF", "DFS"]
+TICKERS = ["COF"]
 START_DATE = "2024-02-01"
 END_DATE = "2025-06-30"
 
@@ -16,16 +16,13 @@ data = yf.download(
 
 close_cols = []
 for tk in TICKERS:
-    try:
-        close_cols.append(data[tk]["Close"].rename(tk))
-    except KeyError:
-        pass
+    close_cols.append(data[tk]["Close"].rename(tk))
 
 prices = pd.concat(close_cols, axis=1).dropna(how="all")
 prices = prices.loc["2024-02-04":"2025-06-18"]
 
 prices.index.name = "Date"
-output_file = "COF_DFS_prices_2024-02-04_2025-06-18.csv"
+output_file = "COF_prices_2024-02-04_2025-06-18.csv"
 prices.to_csv(output_file, sep=";")
 
 print("File created:", output_file)
